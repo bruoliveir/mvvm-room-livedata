b3d39c674360176a82d62282ec5f9f19edbc4d6c
Bruno Oliveira <bru.oliveir@gmail.com>
2019-07-07 18:44:24 -0300

    Adds WordViewModel

---
+++ app/src/main/java/com/example/mvvmroomlivedata/WordViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.mvvmroomlivedata
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class WordViewModel(application: Application) : AndroidViewModel(application) {
+    private val wordRepository by lazy { WordRepository(application) }
+
+    fun getAllWords() {
+        wordRepository.getAll()
+    }
+
+    fun insert(word: Word) {
+        wordRepository.insert(word)
+    }
+}
